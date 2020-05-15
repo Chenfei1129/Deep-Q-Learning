76bfb50b6ee6e8f027256dee6754f3d2f9d42e99
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-05-14 20:21:46 -0700

    Update Deep Q learning First part.py

--- Deep Q learning First part.py
+++ Deep Q learning First part.py
@@ -1,18 +1,6 @@
 import numpy as np
 import random
-'''
-def e_greedy(Q, e):
 
-    actionnumber=len(Q.keys())
-    probability={a:e/actionnumber for a in Q.keys()}
-    greedy=max(Q,key=Q.get)
-    probability[greedy]=1-e+e/actionnumber
-    actionsprobability=list(probability.values())
-    actions=list(Q.keys())
-    action=np.random.choice(actions,1,p=actionsprobability)[0]
-    
-    return action
-'''
 def e_greedy(actionSpace, greedyAction, e):
 	actionNumber = len(actionSpace)
 	probability = {action: e/actionNumber for action in actionSpace}
@@ -20,17 +8,6 @@ def e_greedy(actionSpace, greedyAction, e):
 	action = random.choices(actionSpace, list(probability.values()))[0]
 	return action
 
-
-'''
-def e_greedy_in_Q_learning(Q, actionSpace, phi, s, e):
-	actionNumber = len(actionSpace)
-	probability = {action: e/actionNumber for action in actionSpace}
-	greedy = max(Q(phi(s, action)) for action in actionSpace)
-	probability[greedy] = 1 - e + e/actionNumber
-    action = np.random.choice(actionSpace, 1, p = actionsProbability)[0]
-    return action
-
-'''
 class chooseAction():
 	def __init__(self, actionSpace, Q, phi, e_greedy):
 		self.actionSpace = actionSpace
