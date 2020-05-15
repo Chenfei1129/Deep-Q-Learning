d6df0d2aedd710e2de2e13bb80eb5b235a3ba2fa
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-05-14 21:50:31 -0700

    Update Deep Q learning First part.py

--- Deep Q learning First part.py
+++ Deep Q learning First part.py
@@ -8,14 +8,15 @@ def e_greedy(actionSpace, greedyAction, e):
 	action = random.choices(actionSpace, list(probability.values()))[0]
 	return action
 
-class chooseAction():
+class ChooseAction():
 	def __init__(self, actionSpace, Q, phi, e_greedy):
 		self.actionSpace = actionSpace
 		self.phi = phi
 		self.Q = Q
 		self.e_greedy = e_greedy
 	def __call__(self, s, e):
-		greedyAction = max(self.Q(self.phi(s), action) for action in actionSpace)
+		Q_value = {action: self.Q(self.phi(s), action) for action in actionSpace}
+		greedyAction = max(Q_value, key = Q_value.get)
 		action = self.e_greedy(self.actionSpace, greedyAction, e)
 		return action
 
